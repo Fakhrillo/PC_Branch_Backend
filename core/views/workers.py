76b30a7fd18c9c4f78baff180874e2331195e36b
Fakhrillo <dombit1999@gmail.com>
2024-02-16 15:54:58 +0500

    new auth

--- core/views/workers.py
+++ core/views/workers.py
@@ -5,9 +5,19 @@ from rest_framework.permissions import IsAdminUser
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from environs import Env
+env = Env()
+env.read_env()
+
+from eskiz_sms import EskizSMS
+import redis
+import re
+import random
 
 from ..models import *
 from ..serializers import WorkersSerializer
+
+redis_client = redis.StrictRedis(host='localhost', port=6379)
 
 class WorkersAPI(generics.ListCreateAPIView):
     permission_classes = [IsAdminUser]
@@ -30,16 +40,6 @@ class WorkerDeleteAPI(generics.DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = WorkersSerializer
 
-class GET_TOKEN(APIView):
-    def post(self, request):
-        username = request.data['username']
-
-        user = User.objects.filter(username=username).first()
-        if not user:
-            return Response({"error":"Invalid credentials"}, status=401)
-        refresh = RefreshToken.for_user(user)
-        return Response({"access":str(refresh.access_token), "refresh":str(refresh)})
-    
 class IsWorkerExist(APIView):
     def get(self, request):
         username = request.data['username']
@@ -47,4 +47,35 @@ class IsWorkerExist(APIView):
         if not user:
             return Response({"error":"Invalid credentials"}, status=401)
         else:
-            return Response({"Success":f"{user} is registered and can be accessed!"}, status=200)
+            verification_code = ''.join(random.choice('0123456789') for i in range(6))
+            redis_client.set(username, verification_code, ex=120)
+            send_SMS(username, verification_code)
+            return Response({"success": "Sms code has been sent to the user successfuly!"}, status=200)
+
+class GET_TOKEN(APIView):
+    def post(self, request):
+        verif_code = request.data['verif_code']
+        username = request.data['username']
+        
+        if not username or not verif_code:
+            return Response({"error": "Username and verification code are required"}, status=400)
+            
+        user = User.objects.filter(username=username).first()
+        if not user:
+            return Response({"error":"Invalid credentials"}, status=401)
+        
+        stored_code = redis_client.get(username)
+        if stored_code and stored_code.decode('utf-8') == verif_code:
+
+            refresh = RefreshToken.for_user(user)
+            return Response({"access":str(refresh.access_token), "refresh":str(refresh)}, status=200)
+        else:
+            return Response({"error": "Invalid verification code"}, status=401)
+    
+
+def send_SMS(user_phone, random_code):
+    email = env('email')
+    password = env('password')
+    cleaned_number = re.sub(r'\+', '', user_phone)
+    eskiz = EskizSMS(email=email, password=password)
+    eskiz.send_sms(cleaned_number, f'<#> Your verification code: {random_code}', from_whom='4546', callback_url=None)
