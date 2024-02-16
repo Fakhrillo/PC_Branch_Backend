7cd0125f5dd50e5a5afe5d39c48c68a019b795f6
Fakhrillo <dombit1999@gmail.com>
2024-02-16 15:04:38 +0500

    new changes

--- core/views/camera_d.py
+++ core/views/camera_d.py
@@ -15,7 +15,6 @@ from ..serializers import CameraDSerializer
 
 class IsSuperUser(IsAdminUser):
     def has_permission(self, request, view):
-        print(request.user)
         return bool(request.user and request.user.is_superuser)
 
 class CameraDetailsAPI(generics.ListCreateAPIView):
@@ -96,7 +95,6 @@ class CameraWith24HoursData(APIView):
         end_date_str = request.query_params.get('end_date')
 
         camera_objects = Camera_details.objects.all()
-        combined_details = defaultdict(lambda: {"data": {}})
 
         username = env("USERNAME")
         password = env("PASSWORD")
@@ -104,6 +102,8 @@ class CameraWith24HoursData(APIView):
         
         get_token(username, password)
         headers = {'Authorization': f'Bearer {jwt_access_token}'}
+
+        combined_data = defaultdict(lambda: {"incoming": 0, "outgoing": 0})
 
         for camera_object in camera_objects:
             cam_mxid = camera_object.MxID
@@ -119,46 +119,37 @@ class CameraWith24HoursData(APIView):
 
                 for entry in count_details:
                     created_at = datetime.strptime(entry["created_at"], "%Y-%m-%dT%H:%M:%S.%f%z")
-                    hour = created_at.strftime("%H")
-                    date = created_at.strftime("%Y-%m-%d")
+                    hour = created_at.strftime("%Y-%m-%d %H:00")
+                    combined_data[hour]["incoming"] += entry["incoming"]
+                    combined_data[hour]["outgoing"] += entry["outgoing"]
 
-                    if date not in combined_details[cam_mxid]["data"]:
-                        combined_details[cam_mxid]["data"][date] = {}
+        response_data = {"cameras":[], "data":[]}
 
-                    if hour not in combined_details[cam_mxid]["data"][date]:
-                        combined_details[cam_mxid]["data"][date][hour] = {"incoming": 0, "outgoing": 0}
-
-                    combined_details[cam_mxid]["data"][date][hour]["incoming"] += entry["incoming"]
-                    combined_details[cam_mxid]["data"][date][hour]["outgoing"] += entry["outgoing"]
-
-        response_data = []
-
-        for cam_mxid, details in combined_details.items():
-            camera_info = {
-                "MxID": cam_mxid,
-                "details": {
-                    "located": camera_objects.filter(MxID=cam_mxid).first().located,
-                    "status": camera_objects.filter(MxID=cam_mxid).first().status,
-                    "stream_link": camera_objects.filter(MxID=cam_mxid).first().stream_link,
-                    "created_at": camera_objects.filter(MxID=cam_mxid).first().created_at,
-                },
-                "data": []
+        for hour, counts in combined_data.items():
+            data_entry = {
+                "hour": hour,
+                "incoming": counts["incoming"],
+                "outgoing": counts["outgoing"],
+                "present": counts["incoming"] - counts["outgoing"],
             }
+            response_data["data"].append(data_entry)
 
-            for date, hours_data in details["data"].items():
-                for hour, counts in hours_data.items():
-                    data_entry = {
-                        "date": date,
-                        "hour": hour,
-                        "incoming": counts["incoming"],
-                        "outgoing": counts["outgoing"],
-                        "present": counts["incoming"] - counts["outgoing"],
+        for camera_object in camera_objects:
+                cam_mxid = camera_object.MxID
+                camera_info = {
+                    "MxID": cam_mxid,
+                    "details": {
+                        "located": camera_object.located,
+                        "status": camera_object.status,
+                        "stream_link": camera_object.stream_link,
+                        "created_at": camera_object.created_at,
                     }
-                    camera_info["data"].append(data_entry)
-
-            response_data.append(camera_info)
+                }
+                response_data["cameras"].append(camera_info)
 
         return Response(response_data)
+
+
 
 
 
